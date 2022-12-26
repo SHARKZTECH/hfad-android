493504df8632c7e7a6500e2daa2b1a4e264f62ca
SHARKZTECH <mosenjemudogoz@gmail.com>
2022-12-26 12:01:35 +0300

    first commit

--- app/src/main/java/com/example/workout1/WorkoutListFragment.java
+++ app/src/main/java/com/example/workout1/WorkoutListFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 
 import android.view.LayoutInflater;
@@ -13,56 +12,13 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WorkoutListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class WorkoutListFragment extends ListFragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public WorkoutListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WorkoutListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WorkoutListFragment newInstance(String param1, String param2) {
-        WorkoutListFragment fragment = new WorkoutListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    static interface Listener{
+     interface Listener{
         void itemClicked(int id);
-    };
+    }
     private Listener listener;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
